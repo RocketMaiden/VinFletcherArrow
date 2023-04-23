b499bfef1341e1f86fd96dbe8eb5b43f386663a4
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-04-23 10:10:16 +0300

    index problem fixed

--- VinFletcherArrow/Program.cs
+++ VinFletcherArrow/Program.cs
@@ -6,15 +6,20 @@ int arrowhead;
 int fletching;
 //int shaft;
 int.TryParse(Console.ReadLine(), out arrowhead);
+arrowhead--;
 Console.WriteLine("Chose your fletching type - press 1 for plastic, 2 for turkey feathers, 3 for goose feathers");
 int.TryParse(Console.ReadLine(),out fletching);
+fletching--;
 //Console.WriteLine("Enter your shaft in cm");
 //int.TryParse(Console.ReadLine(), out  shaft);
 
 Arrow arrow = new Arrow(arrowhead, fletching);
-
 Vendor vendor = new Vendor();
+
 Console.WriteLine($"The arrow you wish will cost you {vendor.GetCost(arrow)}");
-Console.WriteLine($"Here is your perfect arrow: {vendor.GetArrow(arrow.arrowHead, arrow.fletching)}");
+
+Arrow purchasedArrow = vendor.GetArrow(arrow.arrowHead, arrow.fletching);
+
+Console.WriteLine($"Here is your purchased arrow: {purchasedArrow.arrowHead}, {purchasedArrow.fletching}");
 
 Console.ReadLine();
