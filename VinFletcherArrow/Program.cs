cf4b047f4564ed10cb659d51f143ec99516e9c39
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-04-23 10:04:41 +0300

    logging purchased arrow

--- VinFletcherArrow/Program.cs
+++ VinFletcherArrow/Program.cs
@@ -15,6 +15,6 @@ Arrow arrow = new Arrow(arrowhead, fletching);
 
 Vendor vendor = new Vendor();
 Console.WriteLine($"The arrow you wish will cost you {vendor.GetCost(arrow)}");
-Console.WriteLine($"Here is your perfect arrow: {vendor.GetArrow}");
+Console.WriteLine($"Here is your perfect arrow: {vendor.GetArrow(arrow.arrowHead, arrow.fletching)}");
 
 Console.ReadLine();
