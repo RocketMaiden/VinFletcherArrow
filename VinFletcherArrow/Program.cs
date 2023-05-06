9de69014bb95e109c37470e7b1092d0b6a9475f3
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-05-06 23:42:21 +0300

    created 2 vendors with different sets of arrows

--- VinFletcherArrow/Program.cs
+++ VinFletcherArrow/Program.cs
@@ -14,12 +14,22 @@ Console.WriteLine("Enter your shaft in cm");
 int.TryParse(Console.ReadLine(), out  length);
 
 Arrow arrow = new Arrow(arrowhead, fletching, length);
-Vendor vendor = new Vendor();
 
-Console.WriteLine($"The arrow you wish will cost you {vendor.GetCost(arrow)}");
+Arrow[] vendor1_arrows = new Arrow[] { new Arrow (ArrowheadType.Obsidian,  Fletching.Plastic, length = 5),
+                                       new Arrow (ArrowheadType.Obsidian, Fletching.Turkey, length = 10),
+                                       new Arrow(ArrowheadType.Obsidian, Fletching.Goose, length = 10)};
+Vendor vendor1 = new Vendor(vendor1_arrows);
 
-Arrow purchasedArrow = vendor.GetArrow(arrow.arrowHead, arrow.fletching);
+Arrow[] vendor2_arrows = new Arrow[] { new Arrow(ArrowheadType.Steel, Fletching.Turkey, length = 5),
+                                       new Arrow(ArrowheadType.Wood, Fletching.Goose, length = 5),
+                                       new Arrow(ArrowheadType.Wood, Fletching.Goose, length = 10),
+                                       new Arrow(ArrowheadType.Wood, Fletching.Plastic, length = 15)};
+Vendor vendor2 = new Vendor(vendor2_arrows);
 
-Console.WriteLine($"Here is your purchased arrow: {purchasedArrow.arrowHead}, {purchasedArrow.fletching}");
+Console.WriteLine("Asking vendor1 if he has an arrow you wish");
+Console.WriteLine($"{vendor1.TryGetArrow(arrow)}");
+
+Console.WriteLine("Asking vendor2 if he has an arrow you wish");
+Console.WriteLine($"{vendor2.TryGetArrow(arrow)}");
 
 Console.ReadLine();
