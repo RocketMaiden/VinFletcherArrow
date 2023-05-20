b9f1abb12c9d80716ae86909bcd794908813d5fb
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-05-20 12:02:31 +0300

    Created Player.cs

---
+++ VinFletcherArrow/Player.cs
@@ -0,0 +1,29 @@
+﻿namespace VinFletcherArrow
+{
+    public class Player
+    {
+        float gold;
+        Arrow[] arrows;
+        int countOfArrows;
+
+        public Player(float gold)
+        {
+            this.gold = gold;
+            arrows = new Arrow[10];
+            countOfArrows = 0;
+        }
+
+        public void BuyArrow(Vendor vendor, Arrow desiredArrow)
+        {
+            float arrowCost = vendor.GetCost(desiredArrow);
+
+            if ((arrowCost < gold) && (vendor.GetArrow(desiredArrow.arrowHead, desiredArrow.fletching, desiredArrow.length) != null))
+            {
+                arrows[countOfArrows] = desiredArrow;
+                countOfArrows++;
+                gold -= arrowCost;
+            }
+        }
+
+    }
+}
